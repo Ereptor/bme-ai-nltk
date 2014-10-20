faed150d8cb77f6a121a3edc02b457900adf8ceb
Milan Marsi <ereptor@Milans-MacBook-Pro.local>
2014-10-20 23:06:01 +0200

    force push to sync with local repo, from now on I'll pull before changes

---
+++ tpcalc.py
@@ -0,0 +1,27 @@
+import os.path
+
+def compare(filename):
+  if not os.path.isfile('knowledgebase.dat'):
+    print('Error: No knowledgebase present, please compile'
+    + ' first.')
+    
+    return
+  
+  knowledgebase = open('knowledgebase.dat', 'r')
+  
+  for line in knowledgebase:
+    data = str.split(line, '\t')
+    word = data[0]
+    occurance = int(data[1])
+    
+    print(word + ' - ' + str(occurance))
+    '''
+    Your job is implementing the comparison 
+    knowledgebase contains the 70 most common words in all the texts
+    word is obviously the words in the knowledgebase
+    and occurance is the number of times it was in the text
+    
+    '''
+    
+  
+  knowledgebase.close()
